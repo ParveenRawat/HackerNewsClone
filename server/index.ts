7c6c177338c8f1f60d56fc78403aac384ab0426e
Parveen Rawat <rawatparveen663@gmail.com>
2025-10-26 16:27:40 +0530

    Added: Hono Error Detection

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+import type { ErrorResponse } from "@/shared/types";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errResponse =
+      err.res ??
+      c.json<ErrorResponse>({
+        success: false,
+        error: err.message,
+        isFormError:
+          err.cause && typeof err.cause === "object" && "form" in err.cause
+            ? err.cause.form === true
+            : false,
+      });
+
+    return errResponse;
+  }
+
+  return c.json<ErrorResponse>({
+    success: false,
+    error:
+      process.env.NODE_ENV === "production"
+        ? "Internal Server Error"
+        : (err.stack ?? err.message),
+  });
 });
 
 export default app;
