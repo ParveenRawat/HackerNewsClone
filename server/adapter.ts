2df18eb812045082e0307e8d1f30a0d4762f6871
Parveen Rawat <rawatparveen663@gmail.com>
2025-10-31 18:02:08 +0530

    Added: User signup,login and logout with lucia

--- server/adapter.ts
+++ server/adapter.ts
@@ -1,7 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import postgres from "postgres";
 import { z } from "zod";
+
+import { sessionTable, userTable } from "./db/schemas/auth";
 
 const EnvSchema = z.object({
   DATABASE_URL: z.string().url(),
@@ -11,4 +14,15 @@ const processEnv = EnvSchema.parse(process.env);
 
 const queryClient = postgres(processEnv.DATABASE_URL);
 
-const db = drizzle(queryClient);
+export const db = drizzle(queryClient, {
+  schema: {
+    user: userTable,
+    session: sessionTable,
+  },
+});
+
+export const adapter = new DrizzlePostgreSQLAdapter(
+  db,
+  sessionTable,
+  userTable,
+);
--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export type SuccessResponse<T = void> = {
   success: true;
   message: string;
@@ -5,6 +7,15 @@ export type SuccessResponse<T = void> = {
 
 export type ErrorResponse = {
   success: false;
-  error: string;
+  error?: string;
   isFormError?: boolean;
 };
+
+export const loginSchema = z.object({
+  username: z
+    .string()
+    .min(3)
+    .max(31)
+    .regex(/^[a-zA-Z0-9_]+$/),
+  password: z.string().min(3).max(255),
+});
